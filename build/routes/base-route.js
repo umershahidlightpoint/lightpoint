361e136f6f65c7358fa7a6cc59301151217546a4
usman-anwar-techverx <usman.anwar@gems.techverx.com>
2019-04-30 12:46:36 +0500

    folder restructured

--- build/routes/base-route.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BASE_USER_ROUTE = "/api/v1/users";
-//# sourceMappingURL=base-route.js.map
--- build/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 8080;
-app_1.default.listen(PORT, () => {
-    console.log(`TypeScript App Running on Port ${PORT}`);
-});
-//# sourceMappingURL=server.js.map
