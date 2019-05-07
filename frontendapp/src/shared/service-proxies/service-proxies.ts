77f6015cb66e0d1b2e7a7d99a4756dd6ed89073a
Fahad Shehzad <fahad.shehzad@gmail.com>
2019-05-07 14:32:03 +0500

    refactored

--- frontendapp/src/shared/service-proxies/service-proxies.ts
+++ frontendapp/src/shared/service-proxies/service-proxies.ts
@@ -30,20 +30,13 @@ export class FinancePocServiceProxy {
         }
         return this.http.get(url, { params }).pipe(map((response: any) => response));
     }
-    groupByCustomer(id) {
-        debugger
-        let url_ = this.baseUrl + "/ledgers/group?fund_id={id}&group_by=customer";
-        url_ = url_.replace('{id}', id);
-        let options_: any = {
-            observe: "response",
-            responseType: "json",
-            headers: new HttpHeaders({
-                "Content-Type": "application/json",
-                "Accept": "application/json"
-            })
-        };
 
-        return this.http.get(url_).pipe(map((response: any) => response));
+    groupByCustomer(id) {
+        const url = this.baseUrl + '/ledgers/group';
+        const params: any = {};
+        params.fund_id = id;
+        params.group_by = 'customer';
+        return this.http.get(url, { params }).pipe(map((response: any) => response));
     }
     getFunds() {
         const url = this.baseUrl + '/funds';
