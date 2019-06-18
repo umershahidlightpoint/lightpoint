f8a14d6108a17a6ad3408e0b2588ccd1ac42e7fc
khizar <khizar@gems.techverx.com>
2019-06-18 17:20:02 +0500

    - Create Home Page - Create Ledger Entry Page - Integrate API in Ledger Entry Page - Draw Data list in table

---
+++ Blazor App/Shared/Account.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sample.Shared
+{
+   public  class Account
+    {
+        // {"id":20,"name":"AT-Account1","accountType":{"id":4,"name":"Asset"}},
+        public long id { get; set; }
+        public string name { get; set; }
+
+     //   public AccountType accountType { get; set; }
+    }
+    public class AccountType
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+
+    }
+}
