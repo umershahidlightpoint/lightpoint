48a62d1e3e48466f2285fe8eb39aeba258294ab6
David Gay <dlattimore@lightpointft.com>
2019-06-28 10:53:26 -0400

    Moved model to common library and fix up other projects

--- services/PostingEngine/Program.cs
+++ services/PostingEngine/Program.cs
@@ -1,26 +1,24 @@
-﻿using ConsoleApp1.Models;
+﻿using LP.Finance.Common.Models;
 using Newtonsoft.Json;
 using PostingEngine;
 using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     // "TradePrice":0.0,"TradeDate":"2019-06-21T00:00:00","Trader":"BMAY","Status":"Executed","Commission":0.0,"Fees":0.0,"NetMoney":0.0,"UpdatedOn":"2019-06-21T14:42:29.697"
-    
+
 
     /// <summary>
     /// Model that knows how to insert / update it's self
     /// keep the model simple
     /// </summary>
 
-    
+
 
     public class AccountToFrom
     {
