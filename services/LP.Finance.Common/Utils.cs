a53411e7196de942e3805d275a770b6179b67942
David Gay <dlattimore@lightpointft.com>
2019-07-02 14:04:09 -0400

    Added some additional helper functions to make accessing the database super easy

--- services/LP.Finance.Common/Utils.cs
+++ services/LP.Finance.Common/Utils.cs
@@ -1,6 +1,8 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -20,13 +22,23 @@ namespace LP.Finance.Common
 
     public class Utils
     {
+        public static object Wrap(object payload)
+        {
+            return new
+            {
+                when = DateTime.Now,
+                by = "",
+                payload = payload,
+            };
+        }
+
         public static object GetFile(string filename)
         {
             var content = "{}";
 
             var currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
 
-            var folder = currentDir + "Data" + Path.DirectorySeparatorChar + $"{filename}.json";
+            var folder = currentDir + "MockData" + Path.DirectorySeparatorChar + $"{filename}.json";
             if (File.Exists(folder))
                 content = File.ReadAllText(folder);
 
@@ -39,14 +51,46 @@ namespace LP.Finance.Common
         {
             var currentDir = System.AppDomain.CurrentDomain.BaseDirectory;
 
-            var folder = currentDir + "Data" + Path.DirectorySeparatorChar + $"{filename}.json";
+            var dir = currentDir + "MockData";
+            var file = dir + Path.DirectorySeparatorChar + $"{filename}.json";
 
-            if (File.Exists(folder))
-                File.Delete(folder);
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir);
+
+            if (File.Exists(file))
+                File.Delete(file);
 
             var result = JsonConvert.SerializeObject(json);
 
-            File.WriteAllText(folder, result);
+            File.WriteAllText(file, result);
+        }
+
+        public static object RunQuery(string connection, string query)
+        {
+            var content = "{}";
+
+            using (var con = new SqlConnection(connection))
+            {
+                var sda = new SqlDataAdapter(query, con);
+                var dataTable = new DataTable();
+                con.Open();
+                sda.Fill(dataTable);
+                con.Close();
+
+                var jsonResult = JsonConvert.SerializeObject(dataTable);
+                content = jsonResult;              
+            }
+
+            dynamic json = JsonConvert.DeserializeObject(content);
+
+            return Utils.Wrap(json);
+        }
+
+        public static object GetTable (string connection, string tablename)
+        {
+            var query = $@"select * from {tablename} nolock";
+
+            return RunQuery(connection, query);
         }
 
     }
