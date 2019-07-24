1c1e8c5877f0fa30903c3c061cea5625780ea6d4
David Gay <dlattimore@lightpointft.com>
2019-07-24 05:22:40 -0700

    Added a new page for reports

--- frontendapp/src/app/app-routing.module.ts
+++ frontendapp/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FinanceGridComponent } from './finance-grid/finance-grid.component';
 import { RunLogsComponent } from './runlogs/runlogs.component';
+import { ReportsComponent } from './reports/reports.component';
 
 import { LedgerFormComponent } from './ledger-form/ledger-form.component';
 import { CreateAccountComponent } from './ledger-form/create-account/create-account.component';
@@ -9,6 +10,7 @@ import { CreateAccountComponent } from './ledger-form/create-account/create-acco
 const routes: Routes = [
   { path: 'journals-ledgers', component: FinanceGridComponent },
   { path: 'runlogs', component: RunLogsComponent },
+  { path: 'reports', component: ReportsComponent },
   { path: 'accounts', component: LedgerFormComponent, 
     children: [
     {
