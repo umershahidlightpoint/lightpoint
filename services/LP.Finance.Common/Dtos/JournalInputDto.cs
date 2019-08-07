b5baad2996582b803ec4b70667cfb6e743bfe8c4
usman-anwar-techverx <usman.anwar@gems.techverx.com>
2019-08-07 19:04:38 +0500

    Implemented Create Journal Entry API.

---
+++ services/LP.Finance.Common/Dtos/JournalInputDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LP.Finance.Common.Dtos
+{
+    public class JournalInputDto
+    {
+        [Required] public int? AccountFrom { get; set; }
+        [Required] public int? AccountTo { get; set; }
+        [Required] public decimal? Value { get; set; }
+        [Required] public string Fund { get; set; }
+    }
+}
