669182dd6eef0fb3a6a18300a48038d9bf8611c0
David Gay <dlattimore@lightpointft.com>
2019-08-16 07:53:54 -0700

    PostingEngine is now a dll, so can be added as a reference to the Finance project, added a new command line app PostingEngineapp to connect to the new DLL

---
+++ services/PostingEngineApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using LP.Finance.Common.Models;
+using Newtonsoft.Json;
+using PostingEngine;
+using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    // "TradePrice":0.0,"TradeDate":"2019-06-21T00:00:00","Trader":"BMAY","Status":"Executed","Commission":0.0,"Fees":0.0,"NetMoney":0.0,"UpdatedOn":"2019-06-21T14:42:29.697"
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PostingEngine.PostingEngine.Start("ITD");
+        }
+    }
+}
