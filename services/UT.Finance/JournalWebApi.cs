edfa7e363cf663eced4836a9e6741994305231c1
Muhammad Avais <avais@gems.techverx.com>
2019-08-29 05:50:12 -0700

    Trial balance report test added

---
+++ services/UT.Finance/JournalWebApi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LP.Finance.Common;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UT.Finance
+{
+    /// <summary>
+    /// Summary description for JournalWebApi
+    /// </summary>
+    [TestClass]
+    public class JournalWebApi
+    {
+        private static readonly string journalsURL = "/api/journal";
+        private static readonly string trialBalanceReportURL = "/api/journal/trialBalanceReport";
+
+        public JournalWebApi()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void GetTrialBalanceReport()
+        {
+            var trialBalanceReport = Utils.GetWebApiData("FinanceWebApi", trialBalanceReportURL);
+
+            Task.WaitAll(trialBalanceReport);
+
+            dynamic result = JsonConvert.DeserializeObject<object>(trialBalanceReport.Result);
+
+            Assert.IsTrue((result.stats.totalDebit * -1) == result.stats.totalCredit, "Total debit and credit are knocking off");
+        }
+    }
+}
