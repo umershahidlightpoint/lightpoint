ea08db374670239bc7a1ad96fc4485f6a28b15ee
Muhammad Avais <avais@gems.techverx.com>
2019-09-03 02:38:40 -0700

    File Management Schema and GET API.

---
+++ services/LP.Finance.WebProxy/WebAPI/FileManagementController.cs
@@ -0,0 +1,25 @@
+﻿using LP.Finance.WebProxy.WebAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace LP.Finance.WebProxy.WebAPI
+{
+    [RoutePrefix("api/fileManagement")]
+    public class FileManagementController : ApiController
+    {
+        private IFileManagementService controller = new FileManagementService();
+
+        // GET api/fileManagement/files
+        [Route("files")]
+        [HttpGet]
+        public object GetFiles(string name = "All")
+        {
+            return controller.GetFiles(name);
+        }
+
+    }
+}
---
+++ services/LP.Finance.WebProxy/WebAPI/Services/FileManagementService.cs
@@ -0,0 +1,31 @@
+﻿using LP.Finance.Common;
+using Newtonsoft.Json;
+using SqlDAL.Core;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.Finance.WebProxy.WebAPI.Services
+{
+    public class FileManagementService : IFileManagementService
+    {
+        private static readonly string connectionString = ConfigurationManager.ConnectionStrings["FinanceDB"].ToString();
+        public SqlHelper sqlHelper = new SqlHelper(connectionString);
+
+        public object GetFiles(string name)
+        {
+            var query = $@"select f.Id, f.Name, f.Path, fa.File_Action_Id, fa.File_Id, fa.Action, fa.Action_Start_Date, fa.Action_End_Date from [file] f
+                        inner join[file_action] fa on f.id = fa.file_id";
+
+            var dataTable = sqlHelper.GetDataTable(query, CommandType.Text);
+            var jsonResult = JsonConvert.SerializeObject(dataTable);
+            dynamic json = JsonConvert.DeserializeObject(jsonResult);
+            return Utils.GridWrap(json);
+        }
+
+    }
+}
