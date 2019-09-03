764b6536cb8a4e948efcfe2b9c386482fdc175cf
Muhammad Avais <avais@gems.techverx.com>
2019-09-03 00:24:31 -0700

    Get journal data test case added.

--- services/UT.Finance/JournalWebApi.cs
+++ services/UT.Finance/JournalWebApi.cs
@@ -5,6 +5,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LP.Finance.Common;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Diagnostics;
+using LP.Finance.Common.Dtos;
 
 namespace UT.Finance
 {
@@ -15,6 +17,7 @@ namespace UT.Finance
     public class JournalWebApi
     {
         private static readonly string journalsURL = "/api/journal";
+        private static readonly string getAllJournalsURL = "/api/journal/data/ALL/?pageNumber=0&pageSize=0";
         private static readonly string trialBalanceReportURL = "/api/journal/trialBalanceReport";
 
         public JournalWebApi()
@@ -33,7 +36,23 @@ namespace UT.Finance
 
             dynamic result = JsonConvert.DeserializeObject<object>(trialBalanceReport.Result);
 
-            Assert.IsTrue((result.stats.totalDebit * -1) == result.stats.totalCredit, "Total debit and credit are knocking off");
+            Assert.IsTrue(result.stats.totalDebit == result.stats.totalCredit, "Total debit and credit are knocking off");
+        }
+
+        [TestMethod]
+        public void GetJournalData()
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var journalData = Utils.GetWebApiData("FinanceWebApi", getAllJournalsURL);
+
+            Task.WaitAll(journalData);
+            sw.Stop();
+            var elapsed = sw.ElapsedMilliseconds / 1000;
+
+            dynamic result = JsonConvert.DeserializeObject<object>(journalData.Result);
+
+            Assert.IsTrue(result.data != null, "Expected result");
         }
     }
 }
