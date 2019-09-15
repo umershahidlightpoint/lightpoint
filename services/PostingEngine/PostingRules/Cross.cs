e91a438881b5a7ca69ffc81b4167d62359248277
David Lattimore <dlattimore@lightpointfs.com>
2019-09-15 10:02:01 -0400

    Intrudction of Cross Rules

---
+++ services/PostingEngine/PostingRules/Cross.cs
@@ -0,0 +1,249 @@
+﻿using LP.Finance.Common.Models;
+using PostingEngine.PostingRules.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PostingEngine.PostingRules
+{
+
+    public class CommonStock : PostingRule, IPostingRule
+    {
+        public bool IsValid(PostingEngineEnvironment env, Transaction element)
+        {
+            return true;
+        }
+
+        public void DailyEvent(PostingEngineEnvironment env, Transaction element)
+        {
+            /*
+             * Are there accruals for this trade, we grab them as needed
+            */
+            throw new NotImplementedException();
+        }
+
+        public void SettlementDateEvent(PostingEngineEnvironment env, Transaction element)
+        {
+            // Retrieve Allocation Objects for this trade
+            var tradeAllocations = env.Allocations.Where(i => i.ParentOrderId == element.ParentOrderId).ToList();
+
+            var debitEntry = tradeAllocations[0].Side == element.Side ? tradeAllocations[0] : tradeAllocations[1];
+            var creditEntry = tradeAllocations[0].Side == element.Side ? tradeAllocations[1] : tradeAllocations[0];
+
+            var accountToFrom = GetFromToAccountOnSettlement(element, debitEntry, creditEntry);
+
+            if ( accountToFrom.To == null || accountToFrom.From == null)
+            {
+                env.AddMessage($"Unable to identify From/To accounts for trade {element.OrderSource} :: {element.Side}");
+                return;
+            }
+
+            new AccountUtils().SaveAccountDetails(env, accountToFrom.From);
+            new AccountUtils().SaveAccountDetails(env, accountToFrom.To);
+
+            // This is the fully loaded value to tbe posting
+            //var netMoney = (element.Quantity * element.TradePrice) + element.Commission + element.Fees;
+
+            double fxrate = 1.0;
+
+            // Lets get fx rate if needed
+            if (!element.SettleCurrency.Equals("USD"))
+            {
+                fxrate = Convert.ToDouble(env.FxRates[element.SettleCurrency].Rate);
+            }
+
+            var moneyUSD = element.NetMoney / fxrate;
+
+            if (element.NetMoney != 0.0)
+            {
+                var debit = new Journal
+                {
+                    Source = debitEntry.LpOrderId,
+                    Account = accountToFrom.From,
+                    When = env.ValueDate,
+                    FxCurrency = element.SettleCurrency,
+                    FxRate = fxrate,
+                    Value = moneyUSD * -1,
+                    GeneratedBy = "system",
+                    Fund = element.Fund,
+                };
+
+                var credit = new Journal
+                {
+                    Source = creditEntry.LpOrderId,
+                    Account = accountToFrom.To,
+                    When = env.ValueDate,
+                    FxCurrency = element.SettleCurrency,
+                    FxRate = fxrate,
+                    Value = moneyUSD,
+                    GeneratedBy = "system",
+                    Fund = element.Fund,
+                };
+
+                env.Journals.AddRange( new [] { debit, credit } );
+
+                //new Journal[] { debit, credit }.Save(env);
+            }
+        }
+
+        private AccountToFrom GetFromToAccount(Transaction element, Transaction debit, Transaction credit)
+        {
+            var type = element.GetType();
+            var accountTypes = AccountType.All;
+
+            var listOfFromTags = new List<Tag> {
+                Tag.Find("SecurityType"),
+                Tag.Find("CustodianCode")
+             };
+
+            var listOfToTags = new List<Tag>
+            {
+                Tag.Find("SecurityType"),
+                Tag.Find("CustodianCode")
+            };
+
+            Account fromAccount = null; // Debiting Account
+            Account toAccount = null; // Crediting Account
+
+            switch (element.Side.ToLowerInvariant())
+            {
+                case "buy":
+                    fromAccount = new AccountUtils().CreateAccount(accountTypes.Where(i => i.Name.Equals("LONG POSITIONS AT COST")).FirstOrDefault(), listOfFromTags, debit);
+                    toAccount = new AccountUtils().CreateAccount(accountTypes.Where(i => i.Name.Equals("DUE FROM/(TO) PRIME BROKERS ( Unsettled Activity )")).FirstOrDefault(), listOfToTags, credit);
+                    break;
+                case "sell":
+                    break;
+                case "short":
+                    fromAccount = new AccountUtils().CreateAccount(accountTypes.Where(i => i.Name.Equals("SHORT POSITIONS-COST")).FirstOrDefault(), listOfFromTags, element);
+                    toAccount = new AccountUtils().CreateAccount(accountTypes.Where(i => i.Name.Equals("DUE FROM/(TO) PRIME BROKERS ( Unsettled Activity )")).FirstOrDefault(), listOfToTags, element);
+                    break;
+                case "cover":
+                    break;
+            }
+
+            return new AccountToFrom
+            {
+                From = fromAccount,
+                To = toAccount
+            };
+        }
+
+        private AccountToFrom GetFromToAccountOnSettlement(Transaction element, Transaction debit, Transaction credit)
+        {
+            var type = element.GetType();
+            var accountTypes = AccountType.All;
+
+            var listOfFromTags = new List<Tag>
+            {
+                Tag.Find("SecurityType"),
+                Tag.Find("CustodianCode")
+            };
+
+            var listOfToTags = new List<Tag> {
+                Tag.Find("SecurityType"),
+                Tag.Find("CustodianCode")
+             };
+
+            Account fromAccount = null; // Debiting Account
+            Account toAccount = null; // Crediting Account
+            switch (element.Side.ToLowerInvariant())
+            {
+                case "buy":
+                    fromAccount = new AccountUtils()
+                        .CreateAccount(accountTypes.Where(i => i.Name.Equals("DUE FROM/(TO) PRIME BROKERS ( Unsettled Activity )")).FirstOrDefault(), listOfFromTags, debit);
+                    toAccount = new AccountUtils()
+                        .CreateAccount(accountTypes.Where(i => i.Name.Equals("DUE FROM/(TO) PRIME BROKERS ( Settled Activity )")).FirstOrDefault(), listOfToTags, credit);
+                    break;
+                case "sell":
+                    break;
+                case "short":
+                    fromAccount = new AccountUtils().CreateAccount(accountTypes.Where(i => i.Name.Equals("DUE FROM/(TO) PRIME BROKERS ( Settled Activity )")).FirstOrDefault(), listOfToTags, element);
+                    toAccount = new AccountUtils().CreateAccount(accountTypes.Where(i => i.Name.Equals("SHORT POSITIONS-COST")).FirstOrDefault(), listOfFromTags, element);
+                    break;
+                case "cover":
+                    break;
+            }
+
+
+            return new AccountToFrom
+            {
+                From = fromAccount,
+                To = toAccount
+            };
+        }
+
+        public void TradeDateEvent(PostingEngineEnvironment env, Transaction element)
+        {
+            // Retrieve Allocation Objects for this trade
+            var tradeAllocations = env.Allocations.Where(i => i.ParentOrderId == element.ParentOrderId).ToList();
+
+            var debitEntry = tradeAllocations[0].Side == element.Side ? tradeAllocations[0] : tradeAllocations[1];
+            var creditEntry = tradeAllocations[0].Side == element.Side ? tradeAllocations[1] : tradeAllocations[0];
+
+            var accountToFrom = GetFromToAccount(element, debitEntry, creditEntry);
+
+            if (accountToFrom.To == null || accountToFrom.From == null)
+            {
+                env.AddMessage($"Unable to identify From/To accounts for trade {element.OrderSource} :: {element.Side}");
+                return;
+            }
+
+            new AccountUtils().SaveAccountDetails(env, accountToFrom.From);
+            new AccountUtils().SaveAccountDetails(env, accountToFrom.To);
+
+            double fxrate = 1.0;
+
+            if ( element.LpOrderId.ToLower().Equals("79e41950a1874755ba5c264c40505464"))
+            {
+
+            }
+            // Lets get fx rate if needed
+            if ( !element.TradeCurrency.Equals("USD"))
+            {
+                fxrate = Convert.ToDouble(env.FxRates[element.TradeCurrency].Rate);
+            }
+
+            if (element.LpOrderId == "a12bde3e8342464b841d8b550d7cba69&EFIX@C")
+            {
+            }
+
+            if (element.NetMoney != 0.0)
+            {
+                var moneyUSD = (element.NetMoney / fxrate);
+                var creditAmount = moneyUSD;
+                var debitAmount = moneyUSD * -1;
+
+                var debitJournal = new Journal
+                {
+                    Source = debitEntry.TradeId,
+                    Account = accountToFrom.From,
+                    When = env.ValueDate,
+                    Value = debitAmount,
+                    FxCurrency = element.TradeCurrency,
+                    FxRate = fxrate,
+                    GeneratedBy = "system",
+                    Fund = element.Fund,
+                };
+
+                var creditJournal = new Journal
+                {
+                    Source = creditEntry.TradeId,
+                    Account = accountToFrom.To,
+                    When = env.ValueDate,
+                    FxCurrency = element.TradeCurrency,
+                    FxRate = fxrate,
+                    Value = creditAmount,
+                    GeneratedBy = "system",
+                    Fund = element.Fund,
+                };
+
+                env.Journals.AddRange(new[] { debitJournal, creditJournal });
+
+                //new SQLBulkHelper().Insert("journal", new[] { debitJournal, creditJournal }, env.Connection);
+
+
+                //new Journal[] { debitJournal, creditJournal }.Save(env);
+            }
+        }
+    }
+}
