5a86ff3143d86fc026f3cf0b0e898bb7d05b0b50
David Gay <dlattimore@lightpointft.com>
2019-09-24 12:20:01 -0700

    Extended Lp.FileProcessing and also added a testing project

--- services/LP.FileProcessing/MetaData/SilverMetaDataForPosition.cs
+++ services/LP.FileProcessing/MetaData/SilverMetaDataForPosition.cs
@@ -9,5 +9,10 @@ namespace LP.FileProcessing.MetaData
         public string Security_ID { get; set; }
         public string Symbol { get; set; }
         public string Security_Currency { get; set; }
+
+        public Dictionary<string, string> mapping = new Dictionary<string, string> {
+            {"Security_ID", "Silver_Security_ID" },
+            {"Symbol", "Silver_Symbol" }
+        };
     }
 }
