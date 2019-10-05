63cadd5bf82f975b8dd4b63e7e959c8a798f4da7
David Gay <dlattimore@lightpointft.com>
2019-10-04 17:08:52 -0700

    Preping for TaxLot / Cost Basis calculations

---
+++ services/LP.Finance.Common/Model/CostBasis.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.Finance.Common.Models
+{
+    public class CostBasisDto : IDbAction, IDbModel
+    {
+        public DateTime BusinessDate { get; set; }
+        public double CostBasis { get; set; }
+        public string Symbol { get; set; }
+        public int SecurityId { get; set; }
+
+        // Get a list of Journal Entries for this trade
+        public static KeyValuePair<string, SqlParameter[]> List(string orderId)
+        {
+            var sql = @"select * from cost_basis where source=@source";
+
+            var sqlParams = new SqlParameter[]
+            {
+                    new SqlParameter("source", orderId),
+            };
+
+            return new KeyValuePair<string, SqlParameter[]>(sql, sqlParams);
+        }
+
+        public KeyValuePair<string, SqlParameter[]> Insert
+        {
+            get
+            {
+                var sql = @"insert into cost_basis
+                            (business_date, security_id, 
+                            symbol, cost_basis) 
+                            values 
+                            (@business_date, @security_id, @symbol, @cost_basis)";
+                var sqlParams = new SqlParameter[]
+                {
+                    new SqlParameter("business_date", BusinessDate),
+                    new SqlParameter("security_id", SecurityId),
+                    new SqlParameter("symbol", Symbol),
+                    new SqlParameter("cost_basis", CostBasis),
+
+
+                };
+
+                return new KeyValuePair<string, SqlParameter[]>(sql, sqlParams);
+            }
+        }
+
+        public KeyValuePair<string, SqlParameter[]> Update
+        {
+            get
+            {
+                var sql = "update journal set a=b, c=d where id = @id";
+                var sqlParams = new SqlParameter[]
+                {
+                    new SqlParameter()
+                };
+
+                return new KeyValuePair<string, SqlParameter[]>(sql, sqlParams);
+            }
+        }
+
+        public KeyValuePair<string, SqlParameter[]> Delete => throw new NotImplementedException();
+
+
+        public DataTable MetaData(SqlConnection connection)
+        {
+            var table = new DataTable();
+
+            // read the table structure from the database
+            var localconnection = new SqlConnection(connection.ConnectionString + ";Password=ggtuser");
+            localconnection.Open();
+            using (var adapter = new SqlDataAdapter($"SELECT TOP 0 business_date, security_id, symbol, cost_basis FROM cost_basis", localconnection))
+            {
+                adapter.Fill(table);
+            };
+            localconnection.Close();
+
+            return table;
+
+        }
+        public void PopulateRow(DataRow row)
+        {
+            row["business_date"] = this.BusinessDate;
+            row["security_id"] = this.SecurityId;
+            row["symbol"] = this.Symbol;
+            row["cost_basis"] = this.CostBasis;
+        }
+    }
+
+}
---
+++ services/LP.Finance.Common/Model/TaxLotStatus.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.Finance.Common.Models
+{
+    public class TaxLotStatus : IDbAction, IDbModel
+    {
+        public int Id { get; set; }
+
+        public string OpenId { get; set; }
+
+        public string Status { get; set; }
+
+        // Get a list of Journal Entries for this trade
+        public static KeyValuePair<string, SqlParameter[]> List(string orderId)
+        {
+            var sql = @"select * from tax_lot_status where source=@source";
+
+            var sqlParams = new SqlParameter[]
+            {
+                    new SqlParameter("source", orderId),
+            };
+
+            return new KeyValuePair<string, SqlParameter[]>(sql, sqlParams);
+        }
+
+        public KeyValuePair<string, SqlParameter[]> Insert
+        {
+            get
+            {
+                var sql = @"insert into tax_lot_status 
+                            (open_id, status) 
+                            values 
+                            (@open_id, @status)";
+                var sqlParams = new SqlParameter[]
+                {
+                    new SqlParameter("open_id", OpenId),
+                    new SqlParameter("statis", Status),
+                };
+
+                return new KeyValuePair<string, SqlParameter[]>(sql, sqlParams);
+            }
+        }
+
+        public KeyValuePair<string, SqlParameter[]> Update
+        {
+            get
+            {
+                var sql = "update journal set a=b, c=d where id = @id";
+                var sqlParams = new SqlParameter[]
+                {
+                    new SqlParameter()
+                };
+
+                return new KeyValuePair<string, SqlParameter[]>(sql, sqlParams);
+            }
+        }
+
+        public KeyValuePair<string, SqlParameter[]> Delete => throw new NotImplementedException();
+
+
+        public DataTable MetaData(SqlConnection connection)
+        {
+            var table = new DataTable();
+
+            // read the table structure from the database
+            var localconnection = new SqlConnection(connection.ConnectionString + ";Password=ggtuser");
+            localconnection.Open();
+            using (var adapter = new SqlDataAdapter($"SELECT TOP 0 open_id, statusFROM tax_lot_status", localconnection))
+            {
+                adapter.Fill(table);
+            };
+            localconnection.Close();
+
+            return table;
+
+        }
+        public void PopulateRow(DataRow row)
+        {
+            row["open_id"] = this.OpenId;
+            row["status"] = this.Status;
+        }
+    }
+
+}
