58d9f61fccc3f31fe97f4cc8478884c02836fd79
David Gay <dlattimore@lightpointft.com>
2019-10-08 04:58:10 -0700

    Added Cost_basis

--- schema/Taxlots.sql
+++ schema/Taxlots.sql
@@ -5,6 +5,24 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
+CREATE TABLE [dbo].[cost_basis](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[business_date] [date] NOT NULL,
+	[symbol] [varchar](100) NOT NULL,
+	[balance] [numeric](18, 9) NOT NULL,
+	[quantity] [numeric](18, 9) NOT NULL,
+	[cost_basis] [numeric](18, 9) NOT NULL,
+	[generated_on] [datetime] NOT NULL,
+ CONSTRAINT [PK_cost_basis] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[cost_basis] ADD  CONSTRAINT [DF_cost_basis_generated_on]  DEFAULT (getdate()) FOR [generated_on]
+GO
+
 CREATE TABLE [dbo].[tax_lot](
 	[id] [int] IDENTITY(1,1) NOT NULL,
 	[closing_lot_id] [varchar](127) NOT NULL,
