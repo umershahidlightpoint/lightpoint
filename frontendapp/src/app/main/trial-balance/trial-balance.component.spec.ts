85d96f9a9ed06101fd6f2d141b2cff540feca023
usman-anwar-techverx <usman.anwar@gems.techverx.com>
2019-10-11 19:20:26 +0500

    Added a Basic Test Case for Trial Balance (In Progress)

--- frontendapp/src/app/main/trial-balance/trial-balance.component.spec.ts
+++ frontendapp/src/app/main/trial-balance/trial-balance.component.spec.ts
@@ -1,27 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppModule } from 'src/app/app.module';
 import { TrialGridExampleComponent } from './trial-balance.component';
+import { AgGridModule } from 'ag-grid-angular';
 
 describe('TrialGridExampleComponent', () => {
   let component: TrialGridExampleComponent;
   let fixture: ComponentFixture<TrialGridExampleComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TrialGridExampleComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [AppModule, AgGridModule.withComponents([])],
+      declarations: []
+    }).compileComponents();
+
     fixture = TestBed.createComponent(TrialGridExampleComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Grid API is Available', () => {
+    fixture.detectChanges();
+    expect(component.gridOptions.api).toBeTruthy();
   });
 
-  
+  it('Check Grouped Data', () => {
+    let done = false;
+    let result = false;
+    setTimeout(() => {
+      component.gridOptions.api.forEachNode(node => {
+        if (node.field === 'AccountCategory' && node.group === true) {
+          result = true;
+        }
+        result = node.expanded === true ? true : false;
+      });
+      done = true;
+    }, 5000);
+  });
 });
