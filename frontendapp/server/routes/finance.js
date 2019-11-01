407e167c098e6f89ca5945d59cbaca2c25a4c0eb
usman-anwar-techverx <usman.anwar@gems.techverx.com>
2019-11-01 19:50:51 +0500

    Added the Post, Put, Delete Events in UI Proxy.

--- frontendapp/server/routes/finance.js
+++ frontendapp/server/routes/finance.js
@@ -1,29 +1,157 @@
 const express = require('express');
-const router = express.Router();
 const http = require('http');
+const router = express.Router();
 
-/* GET api listing. */
+/* GET API Listing */
 router.get('*', (req, res) => {
-  console.log('http://localhost:9092/api' + req.url);
+  console.log('GET :: http://localhost:9092/api' + req.url);
 
-  http.get('http://localhost:9092/api' + req.url, (resp) => {
-    let data = '';
-  
-    // A chunk of data has been recieved.
-    resp.on('data', (chunk) => {
-      data += chunk;
+  http
+    .get('http://localhost:9092/api' + req.url, response => {
+      let data = '';
+
+      // A Chunk of Data has been Recieved
+      response.on('data', chunk => {
+        data += chunk;
+      });
+
+      // The Whole Response has been Received
+      response.on('end', () => {
+        res.send(data);
+      });
+    })
+    .on('error', err => {
+      console.log('Error :: ' + err.message);
+      res.send(err.message);
     });
-  
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
+});
+
+/* POST API */
+router.post('*', (req, res) => {
+  console.log('POST :: http://localhost:9092/api' + req.url);
+
+  const data = JSON.stringify(req.body);
+
+  const options = {
+    hostname: 'localhost',
+    port: 9092,
+    path: '/api' + req.url,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+
+  const request = http.request(options, response => {
+    response.on('data', data => {
       res.send(data);
     });
-  
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-    res.send(err.message)
   });
-  
+
+  request.on('error', err => {
+    console.log('Error :: ' + err.message);
+    res.send(err.message);
+  });
+
+  request.write(data);
+  request.end();
+});
+
+/* PUT API */
+router.put('*', (req, res) => {
+  console.log('PUT :: http://localhost:9092/api' + req.url);
+
+  const data = JSON.stringify(req.body);
+
+  const options = {
+    hostname: 'localhost',
+    port: 9092,
+    path: '/api' + req.url,
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+
+  const request = http.request(options, response => {
+    response.on('data', data => {
+      res.send(data);
+    });
+  });
+
+  request.on('error', err => {
+    console.log('Error :: ' + err.message);
+    res.send(err.message);
+  });
+
+  request.write(data);
+  request.end();
+});
+
+/* PATCH API */
+router.patch('*', (req, res) => {
+  console.log('PUT :: http://localhost:9092/api' + req.url);
+
+  const data = JSON.stringify(req.body);
+
+  const options = {
+    hostname: 'localhost',
+    port: 9092,
+    path: '/api' + req.url,
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+
+  const request = http.request(options, response => {
+    response.on('data', data => {
+      res.send(data);
+    });
+  });
+
+  request.on('error', err => {
+    console.log('Error :: ' + err.message);
+    res.send(err.message);
+  });
+
+  request.write(data);
+  request.end();
+});
+
+/* DELETE API */
+router.delete('*', (req, res) => {
+  console.log('DELETE :: http://localhost:9092/api' + req.url);
+
+  const data = JSON.stringify(req.body);
+
+  const options = {
+    hostname: 'localhost',
+    port: 9092,
+    path: '/api' + req.url,
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+
+  const request = http.request(options, response => {
+    response.on('data', data => {
+      res.send(data);
+    });
+  });
+
+  request.on('error', err => {
+    console.log('Error :: ' + err.message);
+    res.send(err.message);
+  });
+
+  request.write(data);
+  request.end();
 });
 
 module.exports = router;
