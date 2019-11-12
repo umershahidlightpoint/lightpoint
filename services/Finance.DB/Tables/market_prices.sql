4aa16bdbb2a130200cdcb2e57801aec148e9d972
David Gay <dlattimore@lightpointft.com>
2019-11-12 11:34:48 -0500

    Added a new table for pricing, hack for the moment until the right solution is in place

---
+++ services/Finance.DB/Tables/market_prices.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[market_prices]
+(
+	[Id] [int] IDENTITY(1,1) NOT NULL PRIMARY KEY, 
+    [business_date] DATETIME NOT NULL, 
+	[security_id] int not null,
+	[symbol] varchar(20) not null,
+    [event] VARCHAR(10) NOT NULL, 
+    [price] DECIMAL(18,9) Not NULL,
+	[last_updated_by] varchar(20) not null,
+	[last_updated_on] DateTime not null
+)
+Go
+
+ALTER TABLE [dbo].[market_prices] ADD  CONSTRAINT [DF_market_prices_last_updated_on]  DEFAULT (getdate()) FOR [last_updated_on]
+GO
+
