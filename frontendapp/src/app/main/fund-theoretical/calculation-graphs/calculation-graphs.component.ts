918627105e4261697649cff886b4e9dda1137f44
usman-anwar-techverx <usman.anwar@gems.techverx.com>
2019-11-20 12:18:45 +0500

    Added Multi Line Graph in Shared Component.

--- frontendapp/src/app/main/fund-theoretical/calculation-graphs/calculation-graphs.component.ts
+++ frontendapp/src/app/main/fund-theoretical/calculation-graphs/calculation-graphs.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, Input, ChangeDetectorRef, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  ChangeDetectorRef,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { of } from 'rxjs';
 import { delay } from 'rxjs/operators';
@@ -24,14 +31,29 @@ export class CalculationGraphsComponent implements OnInit, OnChanges {
   propIDSingle = 'SingleLineChart';
   divHeight = 180;
   divWidth = '95%';
+  // lineColors = ['#ff6960', '#00bd9a'];
+
+  linePropID = 'line';
+  lineTitle = 'Line Plot';
   lineColors = ['#ff6960', '#00bd9a'];
+  xAxisLabel = 'Date';
+  yAxisLabel = 'Value';
+  divSize = '100%';
+  threshold = 0;
+  lineData = {
+    'Company 1': [
+      { date: '11-15-2019', value: 200 },
+      { date: '11-16-2019', value: -210 },
+      { date: '11-17-2019', value: 300 }
+    ],
+    'Company 2': [
+      { date: '11-15-2019', value: 150 },
+      { date: '11-16-2019', value: 170 },
+      { date: '11-17-2019', value: 120 }
+    ]
+  };
 
-  constructor(
-    private toastrService: ToastrService,
-    private cdRef: ChangeDetectorRef
-    ) {
-
-    }
+  constructor(private toastrService: ToastrService, private cdRef: ChangeDetectorRef) {}
 
   ngOnInit() {}
 
@@ -41,10 +63,9 @@ export class CalculationGraphsComponent implements OnInit, OnChanges {
       let cData;
       currentValue.forEach((element, index) => {
         cData = element.data;
-        if(this.mode === 'single'){
-            this.singleChartData = cData;
-            this.title = element.label;
-            // this.cdRef.detectChanges();
+        if (this.mode === 'single') {
+          this.singleChartData = cData;
+          this.title = element.label;
         } else {
           if (cData.length === 0) {
             this.showChart = false;
@@ -59,7 +80,7 @@ export class CalculationGraphsComponent implements OnInit, OnChanges {
               this.ITDData = cData;
             }
           }
-      }
+        }
       });
       if (cData.length > 0) {
         this.showChart = true;
