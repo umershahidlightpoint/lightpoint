d78d998b2bec6f64d6e953c9b0a77341d430c655
usman-anwar-techverx <usman.anwar@gems.techverx.com>
2019-11-25 20:03:37 +0500

    Code Cleanup.

--- frontendapp/src/app/main/summary/journals-summay-detail/journals-summay-detail.component.ts
+++ frontendapp/src/app/main/summary/journals-summay-detail/journals-summay-detail.component.ts
@@ -37,26 +37,12 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
   private columns: any;
   public rowData: any[] = [];
 
-  hideGrid = false;
   gridOptions: GridOptions;
   pinnedBottomRowData;
   totalRecords = 0;
-  symbol = '';
-  funds: any;
-  accountSearch = { id: undefined };
-  valueFilter: number;
-  sortColum: any;
-  sortDirection: any;
-  page: any;
-  pageSize: any;
-  isDataStreaming = false;
   payloadFilters: any;
 
   ignoreFields = IgnoreFields;
-
-  style = Style;
-
-  styleForHeight = HeightStyle(220);
 
   excelParams = {
     fileName: 'Journals',
@@ -140,7 +126,6 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
             params.successCallback(this.rowData, -1);
             AutoSizeAllColumns(this.gridOptions);
           } else {
-            this.isDataStreaming = false;
           }
         },
         error => {
@@ -151,32 +136,26 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
   };
 
   constructor(
-    private cdRef: ChangeDetectorRef,
     private financeService: FinanceServiceProxy,
-    private dataService: DataService,
     private agGridUtls: AgGridUtils,
     private dataDictionary: DataDictionary
   ) {
-    this.hideGrid = false;
     this.initGird();
   }
 
   ngOnInit() {}
 
-  ngAfterViewInit() {
-    this.dataService.flag$.subscribe(obj => {
-      this.hideGrid = obj;
-      if (!this.hideGrid) {
-        this.getAllData(true);
-      }
-    });
-  }
+  ngAfterViewInit() {}
 
   ngOnChanges(changes: SimpleChanges) {
     const { filters } = changes;
-    this.payloadFilters = filters.currentValue;
-    this.gridOptions.api.setDatasource(this.journalsDataSource);
-    this.totalRecords = 0;
+    if (filters.currentValue !== undefined) {
+      this.payloadFilters = filters.currentValue;
+      this.totalRecords = 0;
+      this.rowData = [];
+      this.gridOptions.api.showLoadingOverlay();
+      this.gridOptions.api.setDatasource(this.journalsDataSource);
+    }
   }
 
   initGird() {
@@ -192,9 +171,17 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
       pivotColumnGroupTotals: 'after',
       pivotRowTotals: 'after',
       suppressColumnVirtualisation: true,
-      onGridReady: params => {
-        // params.api.setDatasource(this.journalsDataSource);
+      suppressHorizontalScroll: false,
+      components: {
+        loadingRenderer: params => {
+          if (params.value !== undefined) {
+            return params.value;
+          } else {
+            return '<img src="../images/loading.gif">';
+          }
+        }
       },
+      onGridReady: params => {},
       onFirstDataRendered: params => {
         params.api.forEachNode(node => {
           node.expanded = true;
@@ -204,7 +191,6 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
       enableFilter: true,
       animateRows: true,
       alignedGrids: [],
-      suppressHorizontalScroll: false,
       defaultColDef: {
         sortable: true,
         resizable: true,
@@ -213,9 +199,6 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
     } as GridOptions;
   }
 
-  /*
-  Drives the columns that will be defined on the UI, and what can be done with those fields
-  */
   customizeColumns(columns: any) {
     const colDefs = [
       ...CommonCols(),
@@ -239,114 +222,5 @@ export class JournalsSummayDetailComponent implements OnInit, AfterViewInit, OnC
     ];
     const cdefs = this.agGridUtls.customizeColumns(colDefs, columns, this.ignoreFields);
     this.gridOptions.api.setColumnDefs(cdefs);
-  }
-
-  getAllData(initialLoad) {
-    // this.isDataStreaming = true;
-    this.symbol = 'ALL';
-    const localThis = this;
-    this.page = 0;
-    this.pageSize = 0;
-    this.accountSearch.id = 0;
-    this.valueFilter = 0;
-    this.sortColum = '';
-    this.sortDirection = '';
-    this.financeService.getFunds().subscribe(result => {
-      const localfunds = result.payload.map(item => ({
-        FundCode: item.FundCode
-      }));
-      localThis.funds = localfunds;
-      localThis.cdRef.detectChanges();
-    });
-
-    // this.getJournalData(1, 10000, initialLoad);
-  }
-
-  getJournalData(pageNumber, pageSize, initialLoad) {
-    this.financeService
-      .getJournals(
-        this.symbol,
-        pageNumber,
-        pageSize,
-        this.accountSearch.id,
-        this.valueFilter,
-        this.sortColum,
-        this.sortDirection
-      )
-      .subscribe(
-        result => {
-          if (result.meta.Total > 0) {
-            pageNumber += 1;
-            this.getJournalData(pageNumber, 10000, false);
-
-            this.columns = result.meta.Columns;
-            this.totalRecords += result.meta.Total;
-            // this.rowData = [];
-            const someArray = [];
-            // tslint:disable-next-line: forin
-            for (const item in result.payload) {
-              const someObject = {};
-              // tslint:disable-next-line: forin
-              for (const i in this.columns) {
-                const field = this.columns[i].field;
-                if (this.columns[i].Type == 'System.DateTime') {
-                  someObject[field] = moment(result.payload[item][field]).format('MM-DD-YYYY');
-                } else {
-                  someObject[field] = result.payload[item][field];
-                }
-              }
-              someArray.push(someObject);
-            }
-            this.customizeColumns(this.columns);
-            this.rowData = this.rowData.concat(someArray as []);
-            if (!initialLoad) {
-              this.gridOptions.api.updateRowData({ add: someArray });
-            } else {
-              this.gridOptions.api.setRowData(this.rowData);
-            }
-
-            const fieldsSum: Array<{ name: string; total: number }> = CalTotal(this.rowData, [
-              { name: 'debit', total: 0 },
-              { name: 'credit', total: 0 },
-              { name: 'Commission', total: 0 },
-              { name: 'Fees', total: 0 },
-              { name: 'TradePrice', total: 0 },
-              { name: 'NetPrice', total: 0 },
-              { name: 'SettleNetPrice', total: 0 },
-              { name: 'NetMoney', total: 0 },
-              { name: 'LocalNetNotional', total: 0 },
-              { name: 'value', total: 0 }
-            ]);
-            this.pinnedBottomRowData = [
-              {
-                source: 'Total Records:' + this.totalRecords,
-                AccountType: '',
-                accountName: '',
-                when: '',
-                SecurityId: 0,
-                debit: Math.abs(fieldsSum[0].total),
-                credit: Math.abs(fieldsSum[1].total),
-                balance: Math.abs(fieldsSum[0].total) - Math.abs(fieldsSum[1].total),
-                Commission: fieldsSum[2].total,
-                Fees: fieldsSum[3].total,
-                TradePrice: fieldsSum[4].total,
-                NetPrice: fieldsSum[5].total,
-                SettleNetPrice: fieldsSum[6].total,
-                NetMoney: fieldsSum[7].total,
-                LocalNetNotional: fieldsSum[8].total,
-                value: fieldsSum[9].total
-              }
-            ];
-            this.gridOptions.api.setPinnedBottomRowData(this.pinnedBottomRowData);
-            this.gridOptions.api.refreshCells();
-            AutoSizeAllColumns(this.gridOptions);
-          } else {
-            this.isDataStreaming = false;
-          }
-        },
-        error => {
-          this.isDataStreaming = false;
-        }
-      );
   }
 }
