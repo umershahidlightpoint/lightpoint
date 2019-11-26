92823e1c87a74cd1d5df4b5884c626644bf5368e
Muhammad Avais <avais@gems.techverx.com>
2019-11-26 12:41:36 +0500

    fx rate management schema

---
+++ services/Finance.DB/Tables/fx_rates_history.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[fx_rates_history]
+(
+	[Id] INT NOT NULL PRIMARY KEY,
+	[fx_rate_id] [int] NOT NULL,
+	[business_date] DATETIME NOT NULL, 
+	[currency] varchar(100) not null,
+    [event] VARCHAR(10) NOT NULL, 
+    [price] DECIMAL(18,9) Not NULL,
+	[last_updated_by] varchar(20) not null,
+	[last_updated_on] DateTime not null,
+	CONSTRAINT [FK_fx_rates_history_To_fx_rates] FOREIGN KEY ([fx_rate_id]) REFERENCES [fx_rates]([Id])
+)
---
+++ services/Finance.DB/Triggers/fx_rate_update_trigger.sql
@@ -0,0 +1,15 @@
+﻿CREATE TRIGGER [fx_rate_update_trigger]
+	ON [dbo].[fx_rates]
+	AFTER UPDATE
+	AS
+	BEGIN
+		INSERT INTO [dbo].[fx_rates_history]
+      ([fx_rate_id]
+      ,[business_date]
+	  ,[currency]
+      ,[event]
+	  ,[price]
+      ,[last_updated_by]
+	  ,[last_updated_on])
+	  select * from deleted
+	END
