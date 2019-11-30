ae865674f65c6470c55e1ffed672f073f76dc500
David Gay <dlattimore@lightpointft.com>
2019-11-30 15:21:18 -0500

    The config for the UI

---
+++ frontendapp/src/config.js
@@ -0,0 +1,8 @@
+var config = {
+    "port": "4000",
+    "remoteServerUrl": 'http://localhost:4000/finance',
+    "referenceDataUrl": 'http://localhost:4000/refdata'
+  };
+  
+module.exports = config;
+  
