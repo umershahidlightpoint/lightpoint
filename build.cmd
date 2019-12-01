d1d4314c4e8379d005b354c3fea372b9c237b306
David Gay <dlattimore@lightpointft.com>
2019-12-01 18:23:51 -0500

    Cleanup, now using config.js to determine the envioronment

--- build.cmd
+++ build.cmd
@@ -1,19 +1,32 @@
-Echo "Git Pull and then build"
+@Echo "Git Pull and then build"
 git pull
 if %ERRORLEVEL% NEQ 0 GOTO Failed
 
 cd services
+
 msbuild LP.Finance.WebProxy
 msbuild LP.ReferenceData.WebProxy
+
 cd ..\
 IF exist distribution/nul ( rmdir /s /q distribution )
 mkdir distribution
+mkdir distribution\Services
+mkdir distribution\Web
+mkdir distribution\UI
 
-mkdir distribution\LP.Finance.WebProxy
-xcopy /q services\LP.Finance.WebProxy\bin\Debug /s distribution\LP.Finance.WebProxy
+REM Services
+mkdir distribution\services\LP.Finance.WebProxy
+xcopy /q services\LP.Finance.WebProxy\bin\Debug /s distribution\services\LP.Finance.WebProxy
 
-mkdir distribution\LP.ReferenceData.WebProxy
-xcopy /q services\LP.ReferenceData.WebProxy\bin\Debug /s distribution\LP.ReferenceData.WebProxy
+mkdir distribution\services\LP.ReferenceData.WebProxy
+xcopy /q services\LP.ReferenceData.WebProxy\bin\Debug /s distribution\services\LP.ReferenceData.WebProxy
+
+REM Web UI
+cd ./frontendapp
+npm install && npm run build
+
+cd ../node
+npm install && npm run copy
 
 Goto Finished
 
