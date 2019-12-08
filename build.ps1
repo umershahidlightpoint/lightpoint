c8edc83a8bbdf97373b5b537e7e2fc4a2995dbf7
atif.rather <atif.rather@DEVELOP01>
2019-12-08 13:03:18 -0500

    Added lp.fileprocessing.sln

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell a0e1530e
+Enter-VsDevShell a0e1530e -StartInPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\"
 Set-Location $psscriptRoot
-msbuild services\LP.FileProcessing /t:"restore;build"
+msbuild services\LP.FileProcessing\LP.FileProcessing.sln /t:"restore;build"
 msbuild services\LP.Finance.WebProxy /t:"restore;build"
 msbuild services\LP.ReferenceData.WebProxy /t:"restore;build"
 msbuild services\PostingEngine.sln  /t:"restore;build"
