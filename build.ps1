2acdb0e74d7483219212bef09de4606a2593f164
atif.rather <atif.rather@DEVELOP01>
2019-12-08 12:30:35 -0500

    Added build.ps1

---
+++ build.ps1
@@ -0,0 +1,16 @@
+Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
+Enter-VsDevShell a0e1530e
+Set-Location $psscriptRoot
+msbuild services\LP.FileProcessing /t:"restore;build"
+msbuild services\LP.Finance.WebProxy /t:"restore;build"
+msbuild services\LP.ReferenceData.WebProxy /t:"restore;build"
+msbuild services\PostingEngine.sln  /t:"restore;build"
+
+Set-Location "$psscriptRoot\frontendapp"
+npm install 
+npm run build 
+npm run deploy
+
+Set-Location "$psscriptRoot\node"
+npm install
+npm run deploy
