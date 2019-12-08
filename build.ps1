8808e267d8f4e0f033c53d3bb7eb7fc18555ffb7
atif.rather <atif.rather@DEVELOP01>
2019-12-08 13:20:51 -0500

    Added error handling in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,10 +1,18 @@
 Import-Module "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\Microsoft.VisualStudio.DevShell.dll"
 Enter-VsDevShell a0e1530e -StartInPath "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\"
 Set-Location $psscriptRoot
-msbuild services\LP.FileProcessing\LP.FileProcessing.sln /t:"restore;build"
-msbuild services\LP.Finance.WebProxy /t:"restore;build"
-msbuild services\LP.ReferenceData.WebProxy /t:"restore;build"
-msbuild services\PostingEngine.sln  /t:"restore;build"
+
+@(
+    "services\LP.FileProcessing\LP.FileProcessing.sln"
+    "services\LP.Finance.WebProxy"
+    "services\LP.ReferenceData.WebProxy"
+    "services\PostingEngine.sln"
+) | Foreach-Object {
+    msbuild $_ /t:"restore;build"
+    if (!$?) {
+        Write-Error "Build failed while building item : $_"
+    }
+}
 
 Set-Location "$psscriptRoot\frontendapp"
 npm install 
