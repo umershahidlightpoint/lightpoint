13720b9026345d3dc0df84867b0bd09623ced917
Momin <momin@gems.techverx.com>
2019-12-13 12:48:59 +0500

    - Add post Request for setting

--- services/LP.Finance.WebProxy/WebAPI/Services/ISettingService.cs
+++ services/LP.Finance.WebProxy/WebAPI/Services/ISettingService.cs
@@ -5,6 +5,7 @@ namespace LP.Finance.WebProxy.WebAPI.Services
     public interface ISettingService
     {
         object GetReportingCurrencies();
+        object AddSetting(SettingInputDto setting);
         object UpdateSetting(SettingInputDto setting);
         object GetSetting();
     }
