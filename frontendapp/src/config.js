98761fdcf995336e1ed2a1dec3c826bf3ed73347
David Gay <dlattimore@lightpointft.com>
2020-01-28 10:42:27 -0500

    Added a new entry in the PostingEngine Config file to determin the client it's being run for

--- frontendapp/src/config.js
+++ frontendapp/src/config.js
@@ -1,9 +1,9 @@
 var config = {
     "port": "4000",
-    //"remoteServerUrl": 'http://sit01:3000/finance',
+    "remoteServerUrl": 'http://sit01:3000/finance',
     "referenceDataUrl": 'http://sit01:3000/refdata',
 
-   "remoteServerUrl": 'http://localhost:9092/api',
+   //"remoteServerUrl": 'http://localhost:9092/api',
    //"referenceDataUrl": 'http://sit01:3000/refdata'
 
   };
--- services/LP.Finance.Common/Model/Event.cs
+++ services/LP.Finance.Common/Model/Event.cs
@@ -3,21 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LP.Finance.Common.Models;
 
 namespace LP.Finance.Common.Model
 {
     public static class Event
     {
-        public static string TRADE_DATE = "tradedate";
-        public static string SETTLEMENT = "settlement";
+        public static readonly string UNREALIZED_CASH_FX = "unrealized-cash-fx";
 
-        public static string REALIZED_PNL = "realizedpnl";
-        public static string UNREALIZED_PNL = "unrealizedpnl";
-        public static string DAILY_UNREALIZED_PNL = "daily-unrealizedpnl";
+        public static readonly string UNREALIZED_FX_TRANSLATION = "unrealized-fx-translation";
 
-        public static string SETTLED_CASH = "settled-cash";
+        public static readonly string TRADE_DATE = "tradedate";
+        public static readonly string SETTLEMENT = "settlement";
 
-        public static string JOURNAL = "journal";
+        public static readonly string REALIZED_PNL = "realizedpnl";
 
+        [Obsolete]
+        public static readonly string UNREALIZED_PNL = "unrealizedpnl";
+
+        public static readonly string DAILY_UNREALIZED_PNL = "daily-unrealizedpnl";
+        public static readonly string REVERSE_UNREALIZED_PNL = "reverse-unrealizedpnl";
+
+        public static readonly string SETTLED_CASH = "settled-cash";
+
+        public static readonly string JOURNAL = "journal";
     }
 }
