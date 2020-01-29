1e2453acaa1e10e84c46163986bf8a5057fe611d
Muhammad Avais <avais@gems.techverx.com>
2020-01-29 19:25:21 +0500

    log table added in db project

---
+++ services/Finance.DB/StoredProcs/InsertLog.sql
@@ -0,0 +1,49 @@
+﻿CREATE procedure [dbo].[InsertLog]
+(@RequestBatch varchar(max) NULL,
+@MachineName varchar(max) NULL,
+@RequestURL varchar(max) NULL,
+@RequestType varchar(max) NULL,
+@Payload varchar(max) NULL,
+@Query varchar(max) NULL,
+@Logged datetime,
+@ElapsedTime int,
+@Level varchar(max) NULL,
+@Message varchar(max) NULL,
+@Logger varchar(max) NULL,
+@Exception varchar(max) NULL)
+as
+
+IF @RequestURL = '' SET @RequestURL = NULL
+IF @RequestType = '' SET @RequestType = NULL
+IF @Payload = '' SET @Payload = NULL
+IF @Query = '' SET @Query = NULL
+IF @Exception = '' SET @Exception = NULL
+
+INSERT INTO [dbo].[Log]
+             ([activity_id]
+           ,[machine_name]
+           ,[request_url]
+           ,[request_type]
+           ,[payload]
+           ,[query]
+           ,[logged]
+           ,[elapsed_time]
+           ,[level]
+           ,[message]
+           ,[logger]
+           ,[exception])
+     VALUES
+           (@RequestBatch
+           ,@MachineName
+		   ,@RequestURL
+		   ,@RequestType
+		   ,@Payload
+		   ,@Query
+           ,@Logged
+		   ,@ElapsedTime
+           ,@Level
+           ,@Message
+           ,@Logger
+           ,@Exception)
+GO
+
---
+++ services/Finance.DB/Tables/log.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[log](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[activity_id] [varchar](300) NULL,
+	[machine_name] [nvarchar](200) NULL,
+	[request_url] [varchar](max) NULL,
+	[request_type] [varchar](10) NULL,
+	[payload] [varchar](max) NULL,
+	[query] [varchar](max) NULL,
+	[logged] [datetime] NULL,
+	[elapsed_time] [int] NULL,
+	[level] [varchar](5) NULL,
+	[message] [nvarchar](max) NULL,
+	[logger] [nvarchar](300) NULL,
+	[exception] [nvarchar](max) NULL,
+ CONSTRAINT [PK_dbo.Log] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
