b5353aedd6b5c97c7607c1cc5c0d535fa3970c2c
Muhammad Avais <avais@gems.techverx.com>
2020-01-31 20:10:43 +0500

    first attempt at generation of excel file and sending it as an attachment on the server side. pretty raw at the momen

---
+++ services/LP.FileProcessing/Report Generation/IReport.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace LP.FileProcessing
+{
+    public interface IReport
+    {
+        void Generate(DataTable dt);
+    }
+}
---
+++ services/LP.FileProcessing/Report Generation/SpreadSheet.cs
@@ -0,0 +1,68 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.FileProcessing.Report_Generation
+{
+    public class SpreadSheet : IReport
+    { 
+        public async void Generate(DataTable dt)
+        {
+            MemoryStream ms;
+            using (ExcelPackage pck = new ExcelPackage())
+            {
+                ExcelWorksheet ws = pck.Workbook.Worksheets.Add("TaxLotReport");
+                ws.Cells["A1"].LoadFromDataTable(dt, true);
+                ms = new MemoryStream(pck.GetAsByteArray());
+                await SendEmailAsync("hammad.zia@techverx.com", "hammad", "hammadzia000@gmail.com", null, null, "TaxLotReport", "this is an excel sheet", ms, false);
+            }
+
+
+        }
+
+        //TODO extract this out to utils
+        public static async Task<bool> SendEmailAsync(string from, string fromName, string to, string cc, string bcc,
+            string subject,
+            string body, MemoryStream attachment, bool isHtml)
+        {
+            try
+            {
+                var message = new MailMessage { From = new MailAddress(@from, fromName) };
+                message.To.Add(new MailAddress(to));
+
+                if (!string.IsNullOrEmpty(cc))
+                {
+                    message.CC.Add(cc);
+                }
+
+                if (!string.IsNullOrEmpty(bcc))
+                {
+                    message.Bcc.Add(bcc);
+                }
+
+                message.Subject = subject;
+                message.Body = body;
+                message.Attachments.Add(new Attachment(attachment, "TaxLotReport.xlsx", "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"));
+                message.IsBodyHtml = isHtml;
+
+                using (var smtp = new SmtpClient())
+                {
+                    await smtp.SendMailAsync(message);
+                    await Task.FromResult(0);
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return false;
+            }
+        }
+    }
+}
