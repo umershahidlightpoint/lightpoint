178c6882fba06913c9dad3a26a5f60eaf8985bf8
Momin <momin@gems.techverx.com>
2020-02-03 18:54:54 +0500

    - Add test cases for routing - Add test cases for Settings

---
+++ frontendapp/src/app/main/not-found/notfound.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { NotfoundComponent } from './notfound/notfound.component';
+
+import { LpToolkitModule } from 'lp-toolkit';
+
+import { NotFoundRoutes } from './notfound.route';
+
+const notFoundComponents = [ NotfoundComponent ];
+
+@NgModule({
+  declarations: [...notFoundComponents],
+  exports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild(NotFoundRoutes),
+    LpToolkitModule,
+  ]
+})
+export class NotFoundModule {}
---
+++ frontendapp/src/app/main/not-found/notfound.route.ts
@@ -0,0 +1,11 @@
+import { Route } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+
+export const NotFoundRoutes: Route[] = [
+    {
+        path: '',
+        component: NotfoundComponent
+
+    }
+]
