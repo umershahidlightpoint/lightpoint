88111f2921733642eb3ca98caae248c6af908720
David Gay <dgay@lightpointft.com>
2020-02-08 07:39:11 -0500

    Uncomment the sc create statements

--- scripts/register.cmd
+++ scripts/register.cmd
@@ -2,5 +2,5 @@ set SERVER=DEMO02
 
 echo %SERVER%
 
-rem sc create LP.Finance.WebProxy binPath="C:\Apps\%SERVER%\PortfolioAccounting\Services\LP.Finance.WebProxy\LP.Finance.WebProxy.exe"
-rem sc create LP.ReferenceData.WebProxy binPath="C:\Apps\%SERVER%\PortfolioAccounting\Services\LP.ReferenceData.WebProxy\LP.ReferenceData.WebProxy.exe"
+sc create LP.Finance.WebProxy binPath="C:\Apps\%SERVER%\PortfolioAccounting\Services\LP.Finance.WebProxy\LP.Finance.WebProxy.exe"
+sc create LP.ReferenceData.WebProxy binPath="C:\Apps\%SERVER%\PortfolioAccounting\Services\LP.ReferenceData.WebProxy\LP.ReferenceData.WebProxy.exe"
