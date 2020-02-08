8bbc459cad3f1f0f224a11331b74f13b6cbc38b9
David Gay <dgay@lightpointft.com>
2020-02-08 07:49:23 -0500

    ssume its deployed for localhost for the moment, will need to change when the location of the services changes

--- frontendapp/src/config.js
+++ frontendapp/src/config.js
@@ -1,7 +1,7 @@
 var config = {
-    "port": "4000",
-    "remoteServerUrl": 'http://sit01:3000/finance',
-    "referenceDataUrl": 'http://sit01:3000/refdata',
+    "port": "3000",
+    "remoteServerUrl": 'http://localhost:3000/finance',
+    "referenceDataUrl": 'http://localhost:3000/refdata',
 
    //"remoteServerUrl": 'http://localhost:9092/api',
    //"referenceDataUrl": 'http://sit01:3000/refdata'
--- node/config.js
+++ node/config.js
@@ -1,5 +1,5 @@
 var config = {
-    "port": "4000",
+    "port": "3000",
     "financeWebApi": 'http://localhost:9092/api',
     "referenceDataWebApi": 'http://localhost:9091/api'
   };
