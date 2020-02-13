e4488883e3ec907c635febf216de5eea4ce91e15
David Gay <dlattimore@lightpointft.com>
2020-02-12 22:20:48 -0500

    Added additional fields to the taxlotstatus component to satisfy client ask for additional elements to be added.

--- frontendapp/src/config.js
+++ frontendapp/src/config.js
@@ -1,11 +1,10 @@
 var config = {
     "port": "3000",
-    "remoteServerUrl": 'http://localhost:3000/finance',
-    "referenceDataUrl": 'http://localhost:3000/refdata',
+    //"remoteServerUrl": 'http://localhost:3000/finance',
+    //"referenceDataUrl": 'http://localhost:3000/refdata',
 
-   //"remoteServerUrl": 'http://localhost:9092/api',
-   //"referenceDataUrl": 'http://sit01:3000/refdata'
-
+   "remoteServerUrl": 'http://localhost:9092/api',
+   "referenceDataUrl": 'http://sit01:3000/refdata'
   };
   
 module.exports = config;
