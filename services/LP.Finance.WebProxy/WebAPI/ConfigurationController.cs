00f49d8c23a9deb4fda1ba522ff45aec623fa3ce
Muhammad Avais <avais@gems.techverx.com>
2020-02-17 18:57:29 +0500

    initial structure for configurations.

---
+++ services/LP.Finance.WebProxy/WebAPI/ConfigurationController.cs
@@ -0,0 +1,23 @@
+﻿using LP.Finance.WebProxy.WebAPI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace LP.Finance.WebProxy.WebAPI
+{
+    [RoutePrefix("api/configuration")]
+    public class ConfigurationController : ApiController
+    {
+        private IConfigurationService controller = new ConfigurationService();
+
+        [HttpGet]
+        public object GetConfigurations(string project)
+        {
+            return controller.GetConfigurations(project);
+        }
+
+    }
+}
---
+++ services/LP.Finance.WebProxy/WebAPI/Services/IConfigurationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP.Finance.WebProxy.WebAPI.Services
+{
+    interface IConfigurationService
+    {
+        object GetConfigurations(string project);
+        object AddOrUpdateConfig();
+    }
+}
