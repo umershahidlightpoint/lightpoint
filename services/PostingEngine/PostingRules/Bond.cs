83df5b543e467e8dd49e3231672a105fad8eb2d9
David Gay <dlattimore@lightpointft.com>
2020-03-04 17:36:19 -0500

    Added in Bond, this is a place holder for the moment until we have a better handle on how a bond is processed

---
+++ services/PostingEngine/PostingRules/Bond.cs
@@ -0,0 +1,40 @@
+﻿using LP.Finance.Common.Models;
+using PostingEngine.Contracts;
+using PostingEngine.PostingRules.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PostingEngine.PostingRules
+{
+    /// <summary>
+    /// Defaults to behave like a common stock for the moment.
+    /// </summary>
+    public class Bond : DefaultPostingRules, IPostingRule
+    {
+        public bool IsValid(PostingEngineEnvironment env, Transaction element)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Run for each day that the Tax Lot remains open / partially closed
+        /// </summary>
+        /// <param name="env"></param>
+        /// <param name="element">Trade we aee interested in</param>
+        public new void DailyEvent(PostingEngineEnvironment env, Transaction element)
+        {
+            base.DailyEvent(env, element);
+        }
+
+        public new void SettlementDateEvent(PostingEngineEnvironment env, Transaction element)
+        {
+            base.SettlementDateEvent(env, element);
+        }
+
+        public new void TradeDateEvent(PostingEngineEnvironment env, Transaction element)
+        {
+            base.TradeDateEvent(env, element);
+        }
+    }
+}
