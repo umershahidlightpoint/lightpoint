6ac878e109d503798d7330ac395318ded215df5d
Muhammad Avais <avais@gems.techverx.com>
2020-03-16 20:00:16 +0500

    db project updated for symbol change.

---
+++ services/Finance.DB/Tables/symbol_change.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[symbol_change]
+(
+	[id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[created_by] VARCHAR(100) NOT NULL, 
+    [created_date] DATETIME NOT NULL, 
+    [last_updated_by] VARCHAR(100) NULL, 
+    [last_updated_date] DATETIME NULL,
+	[old_symbol] VARCHAR(100) NOT NULL,
+	[new_symbol] VARCHAR(100) NOT NULL,
+	[notice_date] DATETIME NOT NULL,
+	[execution_date] DATETIME NOT NULL,
+	[active_flag] BIT NOT NULL DEFAULT 1
+)
