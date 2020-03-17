8ab8eb06d06841e7f5644e3a61ed1d1ae52e7393
Muhammad Avais <avais@gems.techverx.com>
2020-03-17 20:47:51 +0500

    triggers added for symbol change

--- services/Finance.DB/Triggers/symbol_change_insert_trigger.sql
+++ services/Finance.DB/Triggers/symbol_change_insert_trigger.sql
@@ -1,7 +1,18 @@
 ﻿CREATE TRIGGER [symbol_change_insert_trigger]
 	ON [dbo].[symbol_change]
-	FOR DELETE, INSERT, UPDATE
+	AFTER INSERT
 	AS
 	BEGIN
-		SET NOCOUNT ON
+INSERT INTO [dbo].[symbol_change_audit]
+           ([symbol_change_id]
+           ,[created_by]
+           ,[created_date]
+           ,[last_updated_by]
+           ,[last_updated_date]
+           ,[old_symbol]
+           ,[new_symbol]
+           ,[notice_date]
+           ,[execution_date]
+           ,[active_flag])
+select * from inserted
 	END
