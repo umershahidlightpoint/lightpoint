d156ceb4383812c72580bf08a7a51691f5549a95
Muhammad Avais <avais@gems.techverx.com>
2020-03-17 13:33:08 +0500

    db projectr updated for symbol change

---
+++ services/Finance.DB/Tables/symbol_change_audit.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[symbol_change_audit]
+(
+	[id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+	[symbol_change_id] INT NOT NULL,
+	[created_by] VARCHAR(100) NOT NULL, 
+    [created_date] DATETIME NOT NULL, 
+    [last_updated_by] VARCHAR(100) NULL, 
+    [last_updated_date] DATETIME NULL,
+	[old_symbol] VARCHAR(100) NOT NULL,
+	[new_symbol] VARCHAR(100) NOT NULL,
+	[notice_date] DATETIME NOT NULL,
+	[execution_date] DATETIME NOT NULL, 
+	[active_flag] BIT NOT NULL,
+    CONSTRAINT [FK_symbol_change_audit_Tosymbol_change] FOREIGN KEY ([symbol_change_id]) REFERENCES [symbol_change]([id])
+)
---
+++ services/Finance.DB/Triggers/symbol_change_update_trigger.sql
@@ -0,0 +1,7 @@
+﻿CREATE TRIGGER [symbol_change_update_trigger]
+	ON [dbo].[symbol_change]
+	FOR DELETE, INSERT, UPDATE
+	AS
+	BEGIN
+		SET NOCOUNT ON
+	END
