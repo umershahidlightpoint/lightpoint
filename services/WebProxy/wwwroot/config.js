115ba2847446357d3a997330ec0985ee584eff14
David Gay <dlattimore@lightpointft.com>
2020-03-18 06:34:57 -0400

    Removed as should not be checked into git, this should be populated by the deployment process

--- services/WebProxy/wwwroot/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    "port": "3000",
-    "remoteServerUrl": 'https://localhost:5001/finance',
-    "referenceDataUrl": 'https://localhost:5001/refdata',
-  };
-  
-module.exports = config;
-  
