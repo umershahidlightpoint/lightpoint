d036d3240e516620ea30fe50e639adf70dc4d5bf
Muhammad Avais <avais@gems.techverx.com>
2020-03-25 16:52:15 +0500

    ability to fetch log files from different sources.

---
+++ services/LP.Shared/Model/Logs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LP.Shared.Model
+{
+    public class Logs
+    {
+        public string LogName { get; set; }
+        public string LogLocation { get; set; }
+    }
+}
